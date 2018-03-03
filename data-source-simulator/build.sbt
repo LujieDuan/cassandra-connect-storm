8e58452e9bc1682fdf50acf9e4e332b8111ffc3b
LujieDuan <66813185@qq.com>
2018-03-03 12:25:10 -0600

    Add dss.yml

--- data-source-simulator/build.sbt
+++ data-source-simulator/build.sbt
@@ -9,3 +9,8 @@ libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % "2.5.11",
   "com.typesafe.akka" %% "akka-testkit" % "2.5.11" % Test
 )
+
+libraryDependencies += "org.yaml" % "snakeyaml" % "1.20-SNAPSHOT"
+
+
+resolvers += "Sonatype-public" at "http://oss.sonatype.org/content/groups/public/"
--- data-source-simulator/src/main/scala/Main.scala
+++ data-source-simulator/src/main/scala/Main.scala
@@ -1,11 +1,24 @@
+import java.io.{File, FileInputStream, InputStream, InputStreamReader}
+
 import KeySpaceActor.Start
 import akka.actor.{ActorSystem, Props}
+import org.yaml.snakeyaml.Yaml
 
 /**
   *
   */
 object Main extends App {
+
+  val DEFAULT_PATH = "resources/dss.yml"
   val system = ActorSystem("data-source-simulator")
+
+  // Java Yaml Reader
+  val filePath = if (args.length > 0) args(0) else DEFAULT_PATH
+  val yaml = new Yaml()
+  val conf : java.util.LinkedHashMap[String, Object] = yaml.load(new FileInputStream(new File(filePath)))
+
+
+
   val k1 = system.actorOf(Props[KeySpaceActor], "k1")
   k1 ! Start
 }
